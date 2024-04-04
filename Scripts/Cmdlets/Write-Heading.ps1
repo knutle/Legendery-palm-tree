7d2505b7d2dc58bf7986d815d5f170288e3c53e3
Knut Leborg <knut@leborg.co>
2024-04-05 01:45:16 +0200

    add basic heading helper (#2)

---
+++ Scripts/Cmdlets/Write-Heading.ps1
@@ -0,0 +1,38 @@
+﻿function Write-Heading {
+    [CmdletBinding()]
+    param(
+        # Heading text
+        [Parameter(Position = 1)]
+        [string]
+        $Value,
+
+        # Heading level
+        [Parameter()]
+        [ValidateSet("Title", "Subtitle")]
+        [string]
+        $Level = "Title",
+
+        # Reduce amount of spacing
+        [Parameter()]
+        [switch]
+        $Compact = $false
+    )
+
+    process {
+        if ($Level -eq "Title") {
+            Write-Host "#" -ForegroundColor Yellow
+            Write-Host "# $Value" -ForegroundColor Yellow
+            Write-Host "#" -ForegroundColor Yellow
+            Write-Host -NoNewline:$Compact
+
+            return
+        }
+
+        if ($Level -eq "Subtitle") {
+            Write-Host "## $Value" -ForegroundColor Cyan
+            Write-Host -NoNewline:$Compact
+
+            return
+        }
+    }
+}
