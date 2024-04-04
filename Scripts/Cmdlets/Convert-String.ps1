63a886bc18b8ee0eb16fa2c7058be965b8bdd458
Knut Leborg <knut@leborg.co>
2024-04-05 00:52:57 +0200

    implement Convert-String cmdlet (closes #1)

---
+++ Scripts/Cmdlets/Convert-String.ps1
@@ -0,0 +1,32 @@
+﻿function Convert-String {
+    [CmdletBinding()]
+    param(
+        # Input string to convert
+        [Parameter(ValueFromPipeline, Position = 1)]
+        [string]
+        $String,
+
+        # Switch to reverse characters in output string
+        [Parameter()]
+        [switch]
+        $Reverse
+    )
+
+    process {
+        $charArray = $String.ToCharArray()
+
+        $indexedCharList = @()
+        for ($i = 0; $i -lt $charArray.Count; $i++) {
+            $indexedCharList += New-Object PSObject -Property @{
+                "Index" = $i
+                "Item"  = $charArray[$i]
+            }
+        }
+
+        if ($Reverse) {
+            return $indexedCharList | Sort-Object -Property Index -Descending | Join-String -Property Item
+        }
+
+        return $indexedCharList
+    }
+}
