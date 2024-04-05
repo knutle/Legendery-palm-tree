e0b32a5400fc660fccfec2feffb11fdb456ca9e2
Knut Leborg <knut@leborg.co>
2024-04-05 06:13:35 +0200

    implement task 3 logic

--- Scripts/RunTask3Demo.ps1
+++ Scripts/RunTask3Demo.ps1
@@ -2,3 +2,18 @@
 
 Write-Heading "Oppgave 3"
 
+$matchingPaths = Get-ChildItem -Path (Get-AssetPath "Task3") -Filter "*.txt" -Recurse -File | Select-Object -ExpandProperty FullName | Where-Object {
+    (Get-Content -Path $_ -Raw) -like "*Sommer*"
+}
+
+Write-Heading "Fant disse $($matchingPaths.Length) filene som oppfylte kravene: " -Level Subtitle -Compact
+
+$assetParentPath = (Get-AssetPath)
+$matchingPaths | ForEach-Object {
+    [PSCustomObject]@{
+        AssetPath    = $_ -replace "^$assetParentPath/*", ""
+        AbsolutePath = $_
+    }
+} | Format-Table
+
+
